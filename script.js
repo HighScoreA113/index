f3ee72b00d0e4bdc605442c5624c7ad74318e4ca
HighScoreA113 <117530606+HighScoreA113@users.noreply.github.com>
2023-09-26 21:00:40 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const square = document.querySelector('.SQUARE');
-const downButton = document.querySelector('.DOWN');
-
-downButton.addEventListener('click', () => {
-  const currentHeight = square.clientHeight;
-  const newHeight = currentHeight + 50;
-  square.style.height = `${newHeight}px`;
-});
